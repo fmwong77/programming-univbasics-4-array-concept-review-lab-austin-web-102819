ff437151c93faba7fd9a4fbdc0ba6ec76777afac
Mei Wong <fongmeiw@gmail.com>
2019-09-22 05:03:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+  while array.length do
+    if array[counter] == value_to_find
+      return counter
+    end
+    counter += 1
+end
+
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.
 end
 
 def find_min_value(array)
